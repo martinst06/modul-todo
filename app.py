b689353fe0ed7370eb7f2d4d13b2d901c174ee74
martinst06 <mstoyanov@adobe.com>
2024-04-22 00:23:10 +0200

    fixed delete button and added the redirect to finished tasks button

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from models import Task
 app = Flask(__name__)
 
 tasks = []
+done_tasks = []
 
 @app.route('/')
 def index():
@@ -11,7 +12,7 @@ def index():
 
 @app.route('/done-tasks')
 def done():
-    return render_template('done.html')
+    return render_template('done.html', done_tasks=done_tasks)
 
 @app.route('/create_task', methods=['POST'])
 def create_task():
@@ -20,19 +21,27 @@ def create_task():
     tasks.append(task)
     return redirect(url_for('index'))
 
-@app.route('/delete_task/<string:task_id>')
-def delete_task(task_id):
+@app.route('/delete_task_index/<string:task_id>')
+def delete_task_index(task_id):
     global tasks
     tasks = [task for task in tasks if task.id != task_id]
     return redirect(url_for('index'))
 
-@app.route('/mark_done/<string:task_id>')
-def mark_done(task_id):
+@app.route('/delete_task_done/<string:task_id>')
+def delete_task_done(task_id):
+    global done_tasks
+    done_tasks = [task for task in done_tasks if task.id != task_id]
     return redirect(url_for('done'))
 
-@app.route('/finished_tasks/')
-def finished_tasks(task_id):
-    return redirect(url_for('done'))
+@app.route('/mark_done/<string:task_id>')
+def mark_done(task_id):
+    global tasks, done_tasks
+    for task in tasks:
+        if task.id == task_id:
+            done_tasks.append(task)
+            tasks.remove(task)
+            break
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
